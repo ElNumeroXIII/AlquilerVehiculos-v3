c8d700f4a120733a13a3d0b0335133afb47b18af
ElNumeroXIII <elalmacendexiii@gmail.com>
2023-05-16 11:18:03 +0200

    actulizando repo para trabajar en la torre

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/grafica/controladores/VentanaPrincipal.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/grafica/controladores/VentanaPrincipal.java
@@ -1,23 +1,37 @@
 package org.iesalandalus.programacion.alquilervehiculos.vista.grafica.controladores;
 
 import org.iesalandalus.programacion.alquilervehiculos.vista.grafica.utilidades.Controlador;
+import org.iesalandalus.programacion.alquilervehiculos.vista.grafica.utilidades.Controladores;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class VentanaPrincipal extends Controlador{
 
-    @FXML
-    private Button btAceptar;
 
     @FXML
-    void aceptar(ActionEvent event) {
-    	System.out.println("muerete");
+    private Button btAcercaDe;
+	
+    @FXML
+    private Button btSalir;
+
+    @FXML
+    void salir(ActionEvent event) {
+    	Platform.exit();
     }
+    
+    @FXML
+    void abrirAcercaDe(ActionEvent event) {
+    	Controlador VentanaSobre = Controladores.get("vistas/VentanaSobre.fxml", "Ventana Sobre", null);
+    	VentanaSobre.getEscenario().showAndWait();
+    }
+    
+    
     @FXML
     void initialize() {
-    	System.out.println("iniciao");
+    	System.out.println("Iniciada ejecución visual");
     }
 
 }
---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/grafica/controladores/VentanaSobre.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.alquilervehiculos.vista.grafica.controladores;
+
+import org.iesalandalus.programacion.alquilervehiculos.vista.grafica.utilidades.Controlador;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class VentanaSobre extends Controlador {
+
+    @FXML
+    private Button btVolverPrincipal;
+
+    @FXML
+    void volverPrincipal(ActionEvent event) {
+    
+    }
+
+}
